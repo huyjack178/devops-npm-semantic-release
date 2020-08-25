77ad91e9a6b4b1bd78555ce199d8727a7c8661d5
Harrison Nguyen <harrison.nguyen@luminetsoft.com>
2020-08-25 14:06:20 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,46 @@
+module.exports = {
+  plugins: [
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        releaseRules: [
+          {
+            type: 'docs',
+            scope: 'README',
+            release: 'patch',
+          },
+          {
+            type: 'refactor',
+            release: 'patch',
+          },
+          {
+            scope: 'no-release',
+            release: false,
+          },
+        ],
+        parserOpts: {
+          noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES'],
+        },
+      },
+    ],
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
+    '@semantic-release/npm',
+    {
+      path: '@semantic-release/git',
+      assets: ['package.json', 'package-lock.json', 'CHANGELOG.md'],
+      message: 'chore(release): ${nextRelease.version}',
+    },
+  ],
+  branches: [
+    'master',
+    {
+      name: 'beta',
+      prerelease: true,
+    },
+    {
+      name: 'dev-*',
+      prerelease: true,
+    },
+  ],
+};
